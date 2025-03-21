96fa37c0f197b3cb302da06b4b15fc837921d663
91tleg <maxchou0417@gmail.com>
2025-03-20 18:52:58 -0700

    add max analog reading for esp32

--- src/def.h
+++ src/def.h
@@ -1,15 +1,14 @@
 #ifndef DEF_H
 #define DEF_H
 
-#define V_REF 5
-#define ADC_MAX 255
+#define V_IN 5
 #define RESISTOR_VALUE 2500
 #define NUM_SAMPLES 5
 #define LCD_ADDRESS 0x27
 #define LCD_COLUMNS 16
 #define LCD_ROWS 2
-#define I2C_SDA 7
-#define I2C_SCL 6
-#define SENSOR_PIN A0
+#define SENSOR_PIN 34
+#define ESP_ANALOG_MAX 4095
+#define ARDUINO_ANALOG_MAX 1023
 
 #endif
